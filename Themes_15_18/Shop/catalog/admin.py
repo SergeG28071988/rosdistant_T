8d2a6ec5b1d29aca7388a1b63c4a173e95055798
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-03-11 00:29:13 +0300

    Создали модели, зарегистрировали их

--- Themes_15_18/Shop/catalog/admin.py
+++ Themes_15_18/Shop/catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
--- Themes_15_18/Shop/catalog/models.py
+++ Themes_15_18/Shop/catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Производитель", blank=False)
+    country = models.CharField(max_length=100, verbose_name="Страна", blank=False)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    CATEGORY_CHOICES = (
+        ('Процессор', 'Процессор'),
+        ('Материнская плата', 'Материнская плата'),
+        ('Оперативная память', 'Оперативная память'),
+        ('Видеокарта', 'Видеокарта'),
+    )
+
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE, 
+                                     verbose_name="Производитель")
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Цена")    
+    category = models.CharField(max_length=20, verbose_name="Категория", choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='media/images/', verbose_name="Фото товара")
+
+    def __str__(self):
+        return self.name
